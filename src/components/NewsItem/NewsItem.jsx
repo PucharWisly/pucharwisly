64ac5935b83f74ebcf4f3e0ba5a93deff5599622
Shvaika <pucharwisly@gmail.com>
2024-01-26 19:05:27 +0100

    nesitem

--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -6,8 +6,8 @@ import { extractDate } from './date'
 const NewsItem = ({ article, location }) => {
   const imageSrc = article.articleImg?.url ? article.articleImg.url : 'https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQ2i0cFr-RgR4-Gp-9wKW0MWhTne_MQDdnL-jwdJYBtpRT32lLxkzvyh2e4gCdCcSz9SfPtxqnDD6kJoiTdv4arrkfJFzu3vopPtcIXdNY'
   const page = article.page === 'Teren' ? '/teren' : '/aktualnosci/' + article.id
-  let src = article.id === 'clrds6uk6if5d0but4mf0j8s2' ? 'https://goksirgornagrupa.pl/turystyka/' : page
-  // src= article.id === 'clruv2b635onz0bw52fn7lohx' && 'https://itgrudziadz.pl/en/must-see-monuments/'
+  const src = article.id === 'clrds6uk6if5d0but4mf0j8s2' ? 'https://goksirgornagrupa.pl/turystyka/' : article.id === 'clruv2b635onz0bw52fn7lohx' ? 'https://itgrudziadz.pl/en/must-see-monuments/' : page
+  const isOutsidePage = (article.id === 'clrds6uk6if5d0but4mf0j8s2' || article.id === 'clruv2b635onz0bw52fn7lohx') ? true : false
   
   return (
     <div className={css.articleContainer}>
@@ -16,7 +16,7 @@ const NewsItem = ({ article, location }) => {
         <img src={imageSrc} alt="" width='300' height='174'/>
       </div>
       <article className={css.articleItem}>
-        <h3><Link target={article.id === 'clrds6uk6if5d0but4mf0j8s2' ? '_blank':'_self'} className={css.articleRef} to={src} state={{from: location}}>{article.title}</Link></h3>
+        <h3><Link target={isOutsidePage ? '_blank':'_self'} className={css.articleRef} to={src} state={{from: location}}>{article.title}</Link></h3>
       </article>
     </div>
   )
