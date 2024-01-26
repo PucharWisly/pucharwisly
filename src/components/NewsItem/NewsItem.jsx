41893853357ceaad740c4af3c91716a6b6744d06
Shvaika <pucharwisly@gmail.com>
2024-01-26 18:53:08 +0100

    Update NewsItem.jsx

--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -7,7 +7,7 @@ const NewsItem = ({ article, location }) => {
   const imageSrc = article.articleImg?.url ? article.articleImg.url : 'https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQ2i0cFr-RgR4-Gp-9wKW0MWhTne_MQDdnL-jwdJYBtpRT32lLxkzvyh2e4gCdCcSz9SfPtxqnDD6kJoiTdv4arrkfJFzu3vopPtcIXdNY'
   const page = article.page === 'Teren' ? '/teren' : '/aktualnosci/' + article.id
   let src = article.id === 'clrds6uk6if5d0but4mf0j8s2' ? 'https://goksirgornagrupa.pl/turystyka/' : page
-  src= article.id === 'clruv2b635onz0bw52fn7lohx' && 'https://itgrudziadz.pl/en/must-see-monuments/'
+  // src= article.id === 'clruv2b635onz0bw52fn7lohx' && 'https://itgrudziadz.pl/en/must-see-monuments/'
   
   return (
     <div className={css.articleContainer}>
